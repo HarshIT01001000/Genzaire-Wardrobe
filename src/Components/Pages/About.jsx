1cb225ed3c7189caeef316830fa28f3f7706193d
HarshIT01001000 <raiharshit009@gmail.com>
2024-08-04 21:00:36 +0530

    4th commit

--- src/Components/Pages/About.jsx
+++ src/Components/Pages/About.jsx
@@ -5,7 +5,7 @@ function About(){
 
         <>
            <div className="flex justify-center mt-[7rem] mb-[7rem]">
-           <div className=" bg-slate-300 h-[30rem] w-[60rem] text-black font-bold text-1xl flex justify-center items-center text-wrap p-[7rem]">
+           <div className=" bg-slate-300 h-[30rem] w-[60rem] text-black font-bold text-1xl flex justify-center items-center text-wrap p-[2rem] md:p-[7rem]">
          
          At Genzaire Wardrobe, we believe that clothing is a canvas for storytelling, and each piece you wear should carry a narrative that resonates with you.Our commitment goes beyond fashion; we are curators of stories, and our canvas? The Seal T-shirts that bring tales to life..
         Genzaire Wardrobe is more than a clothing brand it's a creative space where we bring stories to life through carefully crafted prints. Our team of artists and designers pour their passion into every design, ensuring that each Seal T-shirt is a wearable masterpiece, a conversation starter, and a piece of art you carry with you.
--- src/Components/Pages/Oldstock.jsx
+++ src/Components/Pages/Oldstock.jsx
@@ -4,11 +4,33 @@ function Oldstock(){
     return(
 
         <>
-       <div className="flex justify-center space-x-8 mt-[5rem] mb-[5rem]">
-       <FeaturedCollectionCard></FeaturedCollectionCard>
-        <FeaturedCollectionCard></FeaturedCollectionCard>
-        <FeaturedCollectionCard></FeaturedCollectionCard>
-       </div>
+      <div className="min-h-screen w-full">
+
+<div className="text-center text-2xl font-semibold lg:text-3xl mt-[3rem] mb-[3rem]">
+  Old Collection
+</div>
+
+{/* collection start here */}
+<div className="flex justify-center items-center rounded-sm">
+  <div className="carousel space-x-2 md:space-x-12">
+    <div className="carousel-item">
+      <FeaturedCollectionCard Item1={"Universality"} 
+      Item3={"https://itsinji.com/cdn/shop/files/D9A34A3C-1918-470E-A0AF-0EFFB958924D_180x.jpg?v=1721982455"} 
+      Item4={"https://itsinji.com/cdn/shop/files/505AE9A2-4479-45DD-855F-052850BBF1FB_180x.jpg?v=1722584425"} />
+    </div>
+    <div className="carousel-item">
+      <FeaturedCollectionCard Item1={"Lifeless innovation"} 
+      Item3={"https://itsinji.com/cdn/shop/files/C5AAD91D-553D-43E5-A2FD-F33CE882CDC3_180x.jpg?v=1722584176"} 
+      Item4={"https://itsinji.com/cdn/shop/files/D9A34A3C-1918-470E-A0AF-0EFFB958924D_180x.jpg?v=1721982455"} />
+    </div>
+    <div className="carousel-item">
+      <FeaturedCollectionCard Item1={"Dejection"}
+      Item3={"https://itsinji.com/cdn/shop/files/505AE9A2-4479-45DD-855F-052850BBF1FB_180x.jpg?v=1722584425"} 
+      Item4={"https://itsinji.com/cdn/shop/files/C5AAD91D-553D-43E5-A2FD-F33CE882CDC3_180x.jpg?v=1722584176"} />
+    </div>
+  </div>
+</div>
+</div>
         </>
     )
 }
