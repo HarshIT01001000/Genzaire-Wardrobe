12b659fa2a9a428b9ba38fe0ff78f4ae7ae8a464
HarshIT01001000 <raiharshit009@gmail.com>
2024-09-22 23:34:06 +0530

    6comit

--- src/Components/PagesReusable/PageBuyCart.jsx
+++ src/Components/PagesReusable/PageBuyCart.jsx
@@ -20,7 +20,7 @@ function PageBuyCart() {
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row">
-        <img src={item3} className="max-w-sm rounded-lg shadow-2xl" alt="Product" />
+        <img src={item3} className="max-w-sm rounded-lg shadow-2xl h-[20rem]" alt="Product" />
         <div className="flex flex-col space-y-5 ml-2">
           <h1 className="text-5xl font-semibold">{item1}</h1>
           <h3 className="text-3xl">{item2}</h3>
@@ -54,10 +54,10 @@ function PageBuyCart() {
                 <div className="text-4xl">+</div>
               </button>
             </div>
-            <button className="btn btn-wide bg-slate-500">Add To Cart</button>
+            <button className="btn bg-slate-500 ">Add To Cart</button>
           </div>
 
-          <button className="btn btn-primary">Buy Now</button>
+          <button className="btn btn-primary ">Buy Now</button>
         </div>
       </div>
     </div>
--- src/Components/homePages/FeaturedCollection.jsx
+++ src/Components/homePages/FeaturedCollection.jsx
@@ -40,12 +40,46 @@ function FeaturedCollection() {
             <div className="carousel-item">
               <FeaturedCollectionCard Item1={"Lifeless innovation"} 
               Item3={"https://itsinji.com/cdn/shop/files/C5AAD91D-553D-43E5-A2FD-F33CE882CDC3_180x.jpg?v=1722584176"} 
-              Item4={"https://itsinji.com/cdn/shop/files/D9A34A3C-1918-470E-A0AF-0EFFB958924D_180x.jpg?v=1721982455"} />
+              Item4={"https://itsinji.com/cdn/shop/files/D9A34A3C-1918-470E-A0AF-0EFFB958924D_180x.jpg?v=1721982455"}
+              item5={
+                <>
+                  <Link
+                    to={{
+                      pathname: "/BuyNow", // Define the route
+                    }}
+                    state={{
+                      item1: "Tshirt4", // Pass the item details as state
+                      item2: "$29.99", // Example price
+                      item3: "https://itsinji.com/cdn/shop/files/22BAC874-7BD8-414B-B13E-CC6CE66A951E_540x.jpg?v=1722584447"
+                    }}
+                  >
+                    <button className="btn bg-slate-400">Buy Now</button>
+                  </Link>
+                </>
+              }
+              />
             </div>
             <div className="carousel-item">
               <FeaturedCollectionCard Item1={"Dejection"}
               Item3={"https://itsinji.com/cdn/shop/files/505AE9A2-4479-45DD-855F-052850BBF1FB_180x.jpg?v=1722584425"} 
-              Item4={"https://itsinji.com/cdn/shop/files/C5AAD91D-553D-43E5-A2FD-F33CE882CDC3_180x.jpg?v=1722584176"} />
+              Item4={"https://itsinji.com/cdn/shop/files/C5AAD91D-553D-43E5-A2FD-F33CE882CDC3_180x.jpg?v=1722584176"}
+              item5={
+                <>
+                  <Link
+                    to={{
+                      pathname: "/BuyNow", // Define the route
+                    }}
+                    state={{
+                      item1: "Tshirt4", // Pass the item details as state
+                      item2: "$29.99", // Example price
+                      item3: "https://itsinji.com/cdn/shop/files/22BAC874-7BD8-414B-B13E-CC6CE66A951E_540x.jpg?v=1722584447"
+                    }}
+                  >
+                    <button className="btn bg-slate-400">Buy Now</button>
+                  </Link>
+                </>
+              }
+              />
             </div>
           </div>
         </div>
